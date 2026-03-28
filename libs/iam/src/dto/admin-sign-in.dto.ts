0f04f48c396593a2d4cd0df121a8d2aab81a2598
Michael Agbeyebiawo <agbeyebiawomicheal@gmail.com>
2026-03-28 12:17:06 +0000

    admin sign-in dto updated

--- libs/iam/src/dto/admin-sign-in.dto.ts
+++ libs/iam/src/dto/admin-sign-in.dto.ts
@@ -1,14 +1,29 @@
 // libs/iam/src/authentication/dto/admin-sign-in.dto.ts
-import { IsEmail, IsStrongPassword, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class AdminSignInDto {
-  @ApiProperty({ example: 'developer@gwaipro.com' })
+  @ApiProperty({ example: 'admin@gwaipro.com' })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'admin@gwaipro' })
-  @IsStrongPassword()
+  @ApiProperty({ example: 'Admin@1234!' })
+  @IsString()
+  @IsNotEmpty()
   @MinLength(8)
   password: string;
 }
+
+// import { IsEmail, IsStrongPassword, MinLength } from 'class-validator';
+// import { ApiProperty } from '@nestjs/swagger';
+
+// export class AdminSignInDto {
+//   @ApiProperty({ example: 'developer@gwaipro.com' })
+//   @IsEmail()
+//   email: string;
+
+//   @ApiProperty({ example: 'admin@gwaipro' })
+//   @IsStrongPassword()
+//   @MinLength(8)
+//   password: string;
+// }
